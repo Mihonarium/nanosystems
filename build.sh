0c53207b1053c09dfe99ab4a9ab92b4dd92e4e37
Mikhail Samin <i@mishasamin.xyz>
2024-05-23 20:32:11 +0000

    edits

--- build.sh
+++ build.sh
@@ -5,3 +5,17 @@ mkdir docs
 python3 chapters_split.py
 
 npm run build
+
+# Define the old and new titles
+old_title='<title data-rh="true">Nanosystems | Nanosystems</title>'
+new_title='<title data-rh="true">Nanosystems by K. Eric Drexler</title>'
+
+# Define the old and new meta tags
+old_meta='<meta data-rh="true" property="og:title" content="Nanosystems | Nanosystems">'
+new_meta='<meta data-rh="true" property="og:title" content="Nanosystems by K. Eric Drexler">'
+
+# Replace the title in the index.html file
+sed -i "s|$old_title|$new_title|g" build/index.html
+
+# Replace the meta tag in the index.html file
+sed -i "s|$old_meta|$new_meta|g" build/index.html
