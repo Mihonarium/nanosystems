dda5a1935d9c8f941086cc874b6982c86b0961f6
Mikhail Samin <i@mishasamin.xyz>
2024-05-23 20:37:20 +0000

    edits

--- build.sh
+++ build.sh
@@ -6,16 +6,16 @@ python3 chapters_split.py
 
 npm run build
 
-# Define the old and new titles
-old_title='<title data-rh="true">Nanosystems | Nanosystems</title>'
+# Define the old and new titles with escaped special characters
+old_title='<title data-rh="true">Nanosystems \| Nanosystems</title>'
 new_title='<title data-rh="true">Nanosystems by K. Eric Drexler</title>'
 
-# Define the old and new meta tags
-old_meta='<meta data-rh="true" property="og:title" content="Nanosystems | Nanosystems">'
+# Define the old and new meta tags with escaped special characters
+old_meta='<meta data-rh="true" property="og:title" content="Nanosystems \| Nanosystems">'
 new_meta='<meta data-rh="true" property="og:title" content="Nanosystems by K. Eric Drexler">'
 
 # Replace the title in the index.html file
-sed -i "s|$old_title|$new_title|g" build/index.html
+sed -i "s|${old_title}|${new_title}|g" build/index.html
 
 # Replace the meta tag in the index.html file
-sed -i "s|$old_meta|$new_meta|g" build/index.html
+sed -i "s|${old_meta}|${new_meta}|g" build/index.html
