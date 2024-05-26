7b7afddbf927372295ab08c0a7ba6aa1a9a0f4fa
Mikhail Samin <i@mishasamin.xyz>
2024-05-26 15:42:27 +0100

    Update multiline_equation_filter.py

--- multiline_equation_filter.py
+++ multiline_equation_filter.py
@@ -4,8 +4,9 @@ import panflute as pf
 def action(elem, doc):
     if isinstance(elem, pf.Math) and elem.format == 'DisplayMath':
         # Ensure multi-line equations are correctly handled
-        if '\\begin' in elem.text:
+        if '\\begin' in elem.text and '\\end' in elem.text:
             elem.text = elem.text.replace('\\begin{equation*}', '\\[').replace('\\end{equation*}', '\\]')
+            elem.text = elem.text.replace('\\begin{align*}', '\\[').replace('\\end{align*}', '\\]')
             elem.text = elem.text.replace('\\begin{aligned}', '\\begin{array}{l}').replace('\\end{aligned}', '\\end{array}')
     return elem
 
